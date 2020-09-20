aa880c5e65e634cbcbb9f641bb8adc5b8a49675c
Innokentii konstantinov <konstantinovksh@gmail.com>
2020-09-20 18:45:55 +0500

    Fix SECRET_KEY and DEBUG

--- bot_audit/settings.py
+++ bot_audit/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'change_me_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ["*"]
 
