f81bafc5ff6147c16e896b9f3c6154f2f37626a2
Innokentii konstantinov <konstantinovksh@gmail.com>
2020-09-20 19:09:44 +0500

    Custom 404 handler

--- bot_audit/urls.py
+++ bot_audit/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('users.urls')),
-    path('', include('polls.urls'))
+    path('', include('polls.urls')),
 ]
+
+handler404 = 'bot_audit.views.handler404'
